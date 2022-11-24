7d626f820036a241725e74f9b346a5801603626d
zhoaib <zhoaibhassan@gmail.com>
2022-11-24 17:09:25 +0600

    brand api

--- index.js
+++ index.js
@@ -13,7 +13,18 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
+    try {
+        const brandsCollection = client.db('kenakati').collection('brands');
 
+        app.get('/brands', async (req, res) => {
+            const query = {};
+            const options = await brandsCollection.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally {
+
+    }
 }
 run().catch(console.log);
 
