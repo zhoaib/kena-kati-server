fe56afb84d0d44098613dcc005c53b53afd0b244
zhoaib <zhoaibhassan@gmail.com>
2022-11-24 16:51:22 +0600

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m5zl65y.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+}
+run().catch(console.log);
+
+app.get('/', async (req, res) => {
+    res.send('kena-kati server is running')
+})
+
+
+app.listen(port, () => console.log(`kena=kati running on port ${port}`))
