fcf01093a29565426dff7e86594c75099e69acfb
zhoaib <zhoaibhassan@gmail.com>
2022-11-26 18:27:32 +0600

    get bookings

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ async function run() {
             res.send(product);
         });
 
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body
             const result = await bookingsCollection.insertOne(booking);
